e6f45689f67df265adbee0401a1e3e22e7bd8ab7
Bartu Onder <onderbartu8@gmail.com>
2026-03-24 00:53:31 +0300

    CIFAR-10 CNN Model Added.

--- CIFAR-10CNN.py
+++ CIFAR-10CNN.py
@@ -34,32 +34,69 @@ test_dataloader = DataLoader(test_data,
                              shuffle=False)
 
 
-class CIFAR10Classifier(nn.Module):
+def calculate_accuracy(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    return (correct / len(y_true)) * 100
+
+def evaluate_model_performance(model: torch.nn.Module, data_loader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, accuracy_function):
+
+    loss = 0
+    accuracy = 0
+
+    model.eval()
+
+    with torch.inference_mode():
+
+        for X, y in data_loader:
+
+            X, y = X.to(device), y.to(device)
+
+            y_pred = model(X)
+            loss += loss_fn(y_pred, y).item()
+            accuracy += accuracy_function(y, y_pred.argmax(dim=1))
+
+        loss /= len(data_loader)
+        accuracy /= len(data_loader)
+
+    return {"model_name": model.__class__.__name__,
+            "model_loss": loss,
+            "model_accuracy": accuracy,
+            }
+
+class CIFAR10CNN(torch.nn.Module):
 
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
 
         super().__init__()
 
-        self.layer_stack = nn.Sequential(
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=input_shape, out_features=hidden_units),
-            nn.Linear(in_features=hidden_units, out_features=output_shape)
+            nn.Linear(in_features=hidden_units * 8 * 8, out_features=output_shape)
         )
 
     def forward(self, x):
-        return self.layer_stack(x)
+        return self.classifier(self.block_2(self.block_1(x)))
 
 
-torch.manual_seed(42)
-model = CIFAR10Classifier(input_shape=3072, hidden_units=128, output_shape=10).to(device)
+model = CIFAR10CNN(input_shape=3, hidden_units=32, output_shape=10).to(device)
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-
-def calculate_accuracy(y_true, y_pred):
-    correct = torch.eq(y_true, y_pred).sum().item()
-    return (correct / len(y_true)) * 100
-
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
 
 epochs = 10
 
@@ -100,114 +137,5 @@ for epoch in range(epochs):
 
     print(f"Epoch: {epoch} | Train Loss: {train_loss:.4f} | Test Loss: {test_loss:.4f} | Test Acc: {test_accuracy:.2f}%")
 
-
-def evaluate_model_performance(model: torch.nn.Module, data_loader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, accuracy_function):
-
-    loss = 0
-    accuracy = 0
-
-    model.eval()
-
-    with torch.inference_mode():
-
-        for X, y in data_loader:
-
-            X, y = X.to(device), y.to(device)
-
-            y_pred = model(X)
-            loss += loss_fn(y_pred, y).item()
-            accuracy += accuracy_function(y, y_pred.argmax(dim=1))
-
-        loss /= len(data_loader)
-        accuracy /= len(data_loader)
-
-    return {"model_name": model.__class__.__name__,
-            "model_loss": loss,
-            "model_accuracy": accuracy,
-            }
-
 model_results = evaluate_model_performance(model=model, data_loader=test_dataloader, loss_fn=loss_fn, accuracy_function=calculate_accuracy)
-
-class CIFAR10ClassifierNonLinear(nn.Module):
-
-    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
-
-        super().__init__()
-
-        self.layer_stack = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=input_shape, out_features=hidden_units),
-            nn.ReLU(),
-            nn.Linear(in_features=hidden_units, out_features=output_shape)
-        )
-
-    def forward(self, x):
-        return self.layer_stack(x)
-
-torch.manual_seed(42)
-model1 = CIFAR10ClassifierNonLinear(input_shape=3072, hidden_units=128, output_shape=10).to(device)
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model1.parameters(), lr=0.001)
-
-epochs = 10
-
-for epoch in range(epochs):
-    train_loss = 0
-    model1.train()
-
-    for batch, (X, y) in enumerate(train_dataloader):
-
-        X, y = X.to(device), y.to(device)
-
-        y_pred = model1(X)
-        loss = loss_fn(y_pred, y)
-        train_loss += loss.item()
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    train_loss /= len(train_dataloader)
-
-    test_loss, test_accuracy = 0, 0
-
-    model1.eval()
-
-    with torch.inference_mode():
-
-        for X, y in test_dataloader:
-
-            X, y = X.to(device), y.to(device)
-
-            test_pred = model1(X)
-            test_loss += loss_fn(test_pred, y).item()
-            test_accuracy += calculate_accuracy(y, test_pred.argmax(dim=1))
-
-        test_loss /= len(test_dataloader)
-        test_accuracy /= len(test_dataloader)
-
-    print(f"Epoch: {epoch} | Train Loss: {train_loss:.4f} | Test Loss: {test_loss:.4f} | Test Acc: {test_accuracy:.2f}%")
-
-model1_results = evaluate_model_performance(model=model1, data_loader=test_dataloader, loss_fn=loss_fn, accuracy_function=calculate_accuracy)
-
 print(model_results)
-print(model1_results)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
